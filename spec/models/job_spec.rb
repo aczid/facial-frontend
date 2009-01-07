878d9a3e5fcc5b67c010e3203b34b6fa658e9166
aczid <aczid@maggie.(none)>
2009-01-07 13:07:56 +0100

    Fixed typo

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,7 +2,7 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Job do
 
-  it "should save itself to the database correctly"
+  it "should save itself to the database correctly" do
     @job = Job.new({:filename => "c3a16-on-ref-f-300x400.csv", :tmp_file => "/home/aczid/workspace/nfi_data/c3a16-on-ref-f-300x400.csv"})
     @job.save
   end
