344d12e1ed3ca461d1db0c08cbf2e414462c3cd9
aczid <aczid@maggie.(none)>
2009-01-18 22:45:34 +0100

    Setting dependency versions through constants

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,17 +1,20 @@
-dependency "merb-action-args", "1.0.4"   # Provides support for querystring arguments to be passed in to controller actions
-dependency "merb-assets", "1.0.4"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
-dependency "merb-cache", "1.0.4"         # Provides your application with caching functions 
-dependency "merb-helpers", "1.0.4"       # Provides the form, date/time, and other helpers
-#dependency "merb-mailer", "1.0.4"        # Integrates mail support via Merb Mailer
-dependency "merb-slices", "1.0.4"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
-dependency "merb-auth", "1.0.4"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
-dependency "merb-param-protection", "1.0.4"
+MERB_VERSION = '<= 1.0.8'
+DM_VERSION = '<= 0.9.8'
+
+dependency "merb-action-args", MERB_VERSION   # Provides support for querystring arguments to be passed in to controller actions
+dependency "merb-assets", MERB_VERSION       # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
+dependency "merb-cache", MERB_VERSION         # Provides your application with caching functions 
+dependency "merb-helpers", MERB_VERSION       # Provides the form, date/time, and other helpers
+#dependency "merb-mailer", MERB_VERSION        # Integrates mail support via Merb Mailer
+dependency "merb-slices", MERB_VERSION        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
+dependency "merb-auth", MERB_VERSION          # An authentication slice (Merb's equivalent to Rails' restful authentication)
+dependency "merb-param-protection", MERB_VERSION
  
-dependency "dm-core", "0.9.8"         # The datamapper ORM
-#dependency "dm-aggregates", "0.9.8"   # Provides your DM models with count, sum, avg, min, max, etc.
-dependency "dm-migrations", "0.9.8"   # Make incremental changes to your database.
-dependency "dm-timestamps", "0.9.8"   # Automatically populate created_at, created_on, etc. when those properties are present.
-#dependency "dm-types", "0.9.8"        # Provides additional types, including csv, json, yaml.
-dependency "dm-validations", "0.9.8"  # Validation framework
-dependency "dm-serializer", "0.9.8"  # Serialization framework
+dependency "dm-core", DM_VERSION         # The datamapper ORM
+#dependency "dm-aggregates", DM_VERSION   # Provides your DM models with count, sum, avg, min, max, etc.
+dependency "dm-migrations", DM_VERSION   # Make incremental changes to your database.
+dependency "dm-timestamps", DM_VERSION   # Automatically populate created_at, created_on, etc. when those properties are present.
+#dependency "dm-types", DM_VERSION        # Provides additional types, including csv, json, yaml.
+dependency "dm-validations", DM_VERSION  # Validation framework
+dependency "dm-serializer", DM_VERSION # Serialization framework
 dependency "dm-paperclip", "2.1.4"
