4f195d7a69c0df66a4f2a98beae9868fa85f71bf
aczid <aczid@maggie.(none)>
2009-01-19 11:12:28 +0100

    Added mathces flag

--- app/controllers/images.rb
+++ app/controllers/images.rb
@@ -1,11 +1,15 @@
 class Images < Application
   # provides :xml, :yaml, :js
+  before :redirect_id
   before :find_job
+  before :add_matches_flag
 
   def index
     @job.prepare
+    if @job.all_images_exist?
     @job.selected_image = @job.sane_name(@job.images[0])
     @job.calculate_matches_for(@job.selected_image)
+    end
     display @images
   end
 
@@ -31,8 +35,17 @@ class Images < Application
       @job = session.user.jobs.get(params[:job_id])
       raise NotFound unless @job
       @job.prepare
+    end
+
+    def add_matches_flag
       @job.num_matches = params[:matches] if params[:matches]
+    end
+
+    def redirect_id
       params[:selected_image] = params[:id] if !params[:selected_image]
+      if params[:id] != params[:selected_image]
+        redirect "/jobs/#{params[:job_id]}/images/#{params[:selected_image]}"
+      end
     end
 
 end # Images
